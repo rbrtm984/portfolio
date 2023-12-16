27c7f0e531f7a33333236b75084d65e5a03a74bc
Rob Mayo <rbrtm984@gmail.com>
2023-12-16 18:17:55 -0500

    trying to get tailwind config to work and resolve npm dependency issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
