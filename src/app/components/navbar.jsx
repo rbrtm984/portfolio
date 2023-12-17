19f509445507b75ab3c71201b821899eb7c18e45
Rob Mayo <rbrtm984@gmail.com>
2023-12-17 13:50:28 -0500

    continued styling navbar, including adding an overlay for mobile viewports

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -25,6 +26,25 @@ const Navbar = () => {
               <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
             </Link>
           </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25}/>
+          </div>
+        </div>
+      </div>
+
+      <div className='fixed left-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500'>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Image src='/navLogo.png' width='87' height='35' alt='/' />
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose size={25}/>
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%]'>{'Developing things'}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
