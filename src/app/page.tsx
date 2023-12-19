23e6f1b8b57d0effbdb05e339b932a8bd8398c42
Rob Mayo <rbrtm984@gmail.com>
2023-12-18 19:50:46 -0500

    added about section, styled image, text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from './components/navbar';
 import { Main } from './components/main';
+import About from './components/about';
 
 export default function Home() {
   return (
     <div className='h-screen'>
       <Navbar />
       <Main />
+      <About />
     </div>
   )
 }
