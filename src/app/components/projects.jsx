e71cf39aea95452a06fd17c1690895c00cbb3687
Rob Mayo <rbrtm984@gmail.com>
2023-12-19 00:16:45 -0500

    changed gradient on project cards

--- src/app/components/projects.jsx
+++ src/app/components/projects.jsx
@@ -10,8 +10,39 @@ function Projects() {
         <h2 className='py-4'>{`What I've Built`}</h2>
         <div className='grid md:grid-cols-2 gap-8'>
 
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#136F63] to-[#4CE0D2]'>
             <Image src={''} alt='/'/> {/* Add image for Kaleidoscope */}
+          </div>
+          <div>
+          Kaleidoscope
+          </div>
+
+          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#136F63] to-[#4CE0D2]'>
+            <Image src={''} alt='/'/> {/* Add image for Ship Log */}
+          </div>
+          <div>
+          Ship Log
+          </div>
+
+          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#136F63] to-[#4CE0D2]'>
+            <Image src={''} alt='/'/> {/* Add image for Easy Court */}
+          </div>
+          <div>
+          Easy Court 
+          </div>
+
+          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#136F63] to-[#4CE0D2]'>
+            <Image src={''} alt='/'/> {/* Add image for Social Hour */}
+          </div>
+          <div>
+          Social Hour 
+          </div>
+
+          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#136F63] to-[#4CE0D2]'>
+            <Image src={''} alt='/'/> {/* Add image for Kart and Kode */}
+          </div>
+          <div>
+          Kart and Kode 
           </div>
 
         </div>
