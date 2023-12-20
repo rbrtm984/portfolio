5cd886ed240f6b36745148862983db02cb037e8d
Rob Mayo <rbrtm984@gmail.com>
2023-12-20 00:48:46 -0500

    added scroll to top button to contact component and edited global styles to account for new color scheme

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const Contact = () => {
   return (
     <div className="w-full lg: h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+        <p className="text-xl tracking-widest uppercase text-[#22AAA1]">
           Contact
         </p>
         <h2 className="py-4">Get in Touch</h2>
@@ -102,9 +104,17 @@ const Contact = () => {
                   <label className="uppercase text-sm py-2">Message</label>
                   <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="10"></textarea>
                 </div>
+                <button className="w-full p-4 text-gray-100 mt-4">Send Message</button>
               </form>
             </div>
           </div>
+        </div>
+        <div className="flex justify-center py-12">
+            <Link href="/">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <HiOutlineChevronDoubleUp className="text-[#136F63]" size={30} />
+                </div>
+            </Link>
         </div>
       </div>
     </div>
