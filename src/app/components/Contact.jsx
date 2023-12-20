81ea0379ac424a0e441147aee6640bfe8b805f1c
Rob Mayo <rbrtm984@gmail.com>
2023-12-20 00:55:11 -0500

    tweaking contact

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -111,7 +111,7 @@ const Contact = () => {
         </div>
         <div className="flex justify-center py-12">
             <Link href="/">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                     <HiOutlineChevronDoubleUp className="text-[#136F63]" size={30} />
                 </div>
             </Link>
