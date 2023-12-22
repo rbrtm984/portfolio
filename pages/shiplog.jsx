21d483f7fc249a4c56c6eae87b0176e2ae80d9b3
Rob Mayo <rbrtm984@gmail.com>
2023-12-21 21:45:41 -0500

    removing ship log

--- pages/shiplog.jsx
+++ pages/shiplog.jsx
@@ -8,14 +8,14 @@ const shiplog = () => {
       <div className=" relative w-screen h-[30vh] lg:h-[40vh]">
 
         <div className="top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10">
-        <Image
+        {/* <Image
             alt=""
             fill
             style={{ 
               objectFit:"cover",
             }}
             src={shiplogImg}
-          />
+          /> */}
           <div className="top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%]">
             <h2 className="py-2">Ship Log</h2>
             <h3>React JS / Tailwind / Firebase</h3>
