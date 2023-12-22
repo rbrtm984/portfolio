07089cee17462b00d516c4c8900d33185dc9073c
Rob Mayo <rbrtm984@gmail.com>
2023-12-21 20:43:38 -0500

    trying to fix styling on shiplog

--- pages/shiplog.jsx
+++ pages/shiplog.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-// import Navbar from "../src/app/components/navbar";
+import Image from "next/image";
+import shiplogImg from "../public/shipLogImg.png";
 
 const shiplog = () => {
   return (
-    <div>
-      {/* <Navbar /> */}
-      <div className="w-screen h-[30vh] lg:w-[40vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10">
+    <div className="w-full pt-[80px]">
+      <div className=" relative w-screen h-[30vh] lg:h-[40vh]">
 
+        <div className="top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10">
+        <Image
+            alt=""
+            fill
+            style={{ 
+              objectFit:"cover",
+            }}
+            src={shiplogImg}
+          />
+          <div className="top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h2 className="py-2">Ship Log</h2>
+            <h3>React JS / Tailwind / Firebase</h3>
+          </div>
         </div>
       </div>
     </div>
