5a0e12a8ad55a945ee80c6b611254cb83ca854ef
Rob Mayo <rbrtm984@gmail.com>
2023-12-21 22:11:59 -0500

    trying to fix by removing link in projectitem

--- src/app/components/projectitem.jsx
+++ src/app/components/projectitem.jsx
@@ -21,11 +21,11 @@ const projectitem = ({ title, backgroundImg, projectUrl }) => {
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">ReactJS</p>
-        <Link href={projectUrl} data-testid="project-url">
+        {/* <Link href={projectUrl} data-testid="project-url">
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More Info
           </p>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
